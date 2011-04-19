78346309634afe5eabcf619fc0ae7ab18eed0a99
Sean Schofield <sean@railsdog.com>
2011-04-19 12:48:00 -0400

    Use Heroku credentials for S3 or local config/s3.yml

--- lib/spree_site.rb
+++ lib/spree_site.rb
@@ -10,3 +10,10 @@ module SpreeSite
     config.to_prepare &method(:activate).to_proc
   end
 end
+
+# Use Heroku ENV variables for S3 credentials when present.  No need to include s3.yml in the git repository.
+if ENV['S3_KEY'] and ENV['S3_SECRET']
+  S3_CREDENTIALS = {:access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET'], :bucket => ENV['S3_BUCKET']}
+else
+  S3_CREDENTIALS = "#{Rails.root}/config/s3.yml"
+end
