8c6f31d4d4c3318adfca0530085ccd076b144b0c
Sean Schofield <sean@railsdog.com>
2011-09-29 15:03:53 -0400

    Skip preference setting if migrations have not been run yet

--- config/initializers/site_preferences.rb
+++ config/initializers/site_preferences.rb
@@ -1,7 +1,9 @@
-Spree::Config.set(:homepage_groups => 'Slingbox,Satellite Radios,Boomboxes,Accessories,Internet Radios,Vehicle Installation')
-Spree::Config.set(:site_name => 'Rails Dog Radio')
+if ActiveRecord::Base.connection.tables.include?('preferences')
+  Spree::Config.set(:homepage_groups => 'Slingbox,Satellite Radios,Boomboxes,Accessories,Internet Radios,Vehicle Installation')
+  Spree::Config.set(:site_name => 'Rails Dog Radio')
 
-Spree::ActiveShipping::Config.set(:origin_country => "US")
-Spree::ActiveShipping::Config.set(:origin_state => "MD")
-Spree::ActiveShipping::Config.set(:origin_city => "Chevy Chase")
-Spree::ActiveShipping::Config.set(:origin_zip => "20815")
+  Spree::ActiveShipping::Config.set(:origin_country => "US")
+  Spree::ActiveShipping::Config.set(:origin_state => "MD")
+  Spree::ActiveShipping::Config.set(:origin_city => "Chevy Chase")
+  Spree::ActiveShipping::Config.set(:origin_zip => "20815")
+end
