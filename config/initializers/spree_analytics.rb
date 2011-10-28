dd12535fa6191cea7148e497f76fdd27421a1be0
cmar <chris@spreecommerce.com>
2011-10-28 08:20:34 -0400

    using production spree analytics

--- config/initializers/spree_analytics.rb
+++ config/initializers/spree_analytics.rb
@@ -1,5 +1,8 @@
-SpreeAnalytics.app_id = 126
-SpreeAnalytics.site_id = 318
-SpreeAnalytics.token = "2de3eca96bcbe54d72b5a79281d5bf73"
-SpreeAnalytics.api_url = 'https://test-api.jirafe.com/v1'
-SpreeAnalytics.data_url = 'test-data.jirafe.com'
+# SpreeAnalytics.app_id = 126
+# SpreeAnalytics.site_id = 318
+# SpreeAnalytics.token = "2de3eca96bcbe54d72b5a79281d5bf73"
+# SpreeAnalytics.api_url = 'https://test-api.jirafe.com/v1'
+# SpreeAnalytics.data_url = 'test-data.jirafe.com'
+SpreeAnalytics.app_id = 2248
+SpreeAnalytics.site_id = 25273
+SpreeAnalytics.token = "7b59836c9585fc82c09c0a1436292987"
