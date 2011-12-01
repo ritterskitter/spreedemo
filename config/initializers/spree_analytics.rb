19021ca089cb5f613ad4cc92267c9ebb2644d683
John Dyer <lbrapid@gmail.com>
2011-12-01 11:41:39 -0500

    WIP

--- config/initializers/spree_analytics.rb
+++ config/initializers/spree_analytics.rb
@@ -3,6 +3,6 @@
 # SpreeAnalytics.token = "2de3eca96bcbe54d72b5a79281d5bf73"
 # SpreeAnalytics.api_url = 'https://test-api.jirafe.com/v1'
 # SpreeAnalytics.data_url = 'test-data.jirafe.com'
-SpreeAnalytics.app_id = 2248
-SpreeAnalytics.site_id = 25861
-SpreeAnalytics.token = "7b59836c9585fc82c09c0a1436292987"
+#SpreeAnalytics.app_id = 2248
+#SpreeAnalytics.site_id = 25861
+#SpreeAnalytics.token = "7b59836c9585fc82c09c0a1436292987"
