1bb2d9e724380e2b0e79d78584e5fb7d3558e541
John Dyer <lbrapid@gmail.com>
2011-12-07 13:26:28 -0500

    Fix issue with preferences

--- config/initializers/site_preferences.rb
+++ config/initializers/site_preferences.rb
@@ -1,9 +1,14 @@
-if ActiveRecord::Base.connection.tables.include?('preferences')
-  Spree::Config.set(:homepage_groups => 'Slingbox,Satellite Radios,Boomboxes,Accessories,Internet Radios,Vehicle Installation')
-  Spree::Config.set(:site_name => 'Rails Dog Radio')
+class RailsdogRadio::Configuration < Spree::Preferences::Configuration
+  preference :homepage_groups, :default => ''
+end
 
-  Spree::ActiveShipping::Config.set(:origin_country => "US")
-  Spree::ActiveShipping::Config.set(:origin_state => "MD")
-  Spree::ActiveShipping::Config.set(:origin_city => "Chevy Chase")
-  Spree::ActiveShipping::Config.set(:origin_zip => "20815")
+RailsdogRadio::Config = RailsdogRadio::Configuration.new
+RailsdogRadio::Config.homepage_groups = 'Slingbox,Satellite Radios,Boomboxes,Accessories,Internet Radios,Vehicle Installation'
+
+if ActiveRecord::Base.connection.tables.include?('spree_preferences')
+  Spree::Config.set(:site_name => 'Rails Dog Radio')
+#   Spree::ActiveShipping::Config.set(:origin_country => "US")
+#   Spree::ActiveShipping::Config.set(:origin_state => "MD")
+#   Spree::ActiveShipping::Config.set(:origin_city => "Chevy Chase")
+#   Spree::ActiveShipping::Config.set(:origin_zip => "20815")
 end
