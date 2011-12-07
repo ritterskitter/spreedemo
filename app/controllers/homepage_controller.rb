9a008645b60aa74594865d11b9a938c3791fe346
John Dyer <lbrapid@gmail.com>
2011-12-07 13:26:18 -0500

    Properly namespace homepage view/controller

--- app/controllers/homepage_controller.rb
+++
@@ -1,9 +0,0 @@
-class HomepageController < Spree::BaseController
-  helper 'spree/products'
-
-  def show
-    @product_groups = Spree::Config[:homepage_groups].split(',').map { |name| Spree::ProductGroup.find_by_name(name) }.compact
-  end
-
-end
-
---
+++ app/controllers/spree/homepage_controller.rb
@@ -0,0 +1,10 @@
+class Spree::HomepageController < Spree::BaseController
+  helper 'spree/products'
+
+  def show
+    binding.pry
+    @product_groups = RailsdogRadio::Config[:homepage_groups].split(',').map { |name| Spree::ProductGroup.find_by_name(name) }.compact
+  end
+
+end
+
