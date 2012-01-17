bda6ce8bbf7c50b8fd7daf6fd2973bc1d093a7eb
John Dyer <lbrapid@gmail.com>
2012-01-17 15:49:19 -0500

    Fix remaining preference issues

--- config/initializers/site_preferences.rb
+++ config/initializers/site_preferences.rb
@@ -2,13 +2,19 @@ class RailsdogRadio::Configuration < Spree::Preferences::Configuration
   preference :homepage_groups, :default => ''
 end
 
+Spree::Config.class_eval do
+  preference :paypal_express_local_confirm, :boolean, :default => true
+end
+
+Spree::ActiveShipping::Config = Spree::ActiveShippingConfiguration.new
+
 RailsdogRadio::Config = RailsdogRadio::Configuration.new
 RailsdogRadio::Config.homepage_groups = 'Slingbox,Satellite Radios,Boomboxes,Accessories,Internet Radios,Vehicle Installation'
 
 if ActiveRecord::Base.connection.tables.include?('spree_preferences')
   Spree::Config.set(:site_name => 'Rails Dog Radio')
-#   Spree::ActiveShipping::Config.set(:origin_country => "US")
-#   Spree::ActiveShipping::Config.set(:origin_state => "MD")
-#   Spree::ActiveShipping::Config.set(:origin_city => "Chevy Chase")
-#   Spree::ActiveShipping::Config.set(:origin_zip => "20815")
+  Spree::ActiveShipping::Config.set(:origin_country => "US")
+  Spree::ActiveShipping::Config.set(:origin_state => "MD")
+  Spree::ActiveShipping::Config.set(:origin_city => "Chevy Chase")
+  Spree::ActiveShipping::Config.set(:origin_zip => "20815")
 end
